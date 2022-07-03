dfc39c28fd154e316a8f5d91d26b5a42d2c214f4
Zayra Pizaña <zayra.valiktan@gmail.com>
2022-07-02 22:36:00 -0500

    second image

--- src/components/home/HomeScreen.jsx
+++ src/components/home/HomeScreen.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import InputHome from './InputHome'
+import Logo from '../../assets/img/logo.png'
 
 const HomeScreen = ({ setIsLogged }) => {
   return (
     <div className='container_home'>
-      <img src="/src/assets/img/logo.png" alt="" />
+      <img src={Logo} alt="" />
       <h2 className='home_subtitle'><span className='home_subtitle-span'>Welcome !</span> Trainer</h2>
 
       <InputHome setIsLogged={setIsLogged} />
