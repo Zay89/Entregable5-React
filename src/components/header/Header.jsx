bed8c6eedc04d21f256abd44507ea15ed0ad0362
Zayra Pizaña <zayra.valiktan@gmail.com>
2022-07-02 22:30:55 -0500

    image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Gaming1 from '../../assets/img/gaming1.png'
+import Ash from '../../assets/img/ash.png'
 
 const Header = () => {
   return (
     <header className='header'> 
        
-        <img src="/src/assets/img/gaming1.png" alt="" />
-        <img src="/src/assets/img/ash.png"alt="" />
+        <img src={Gaming1} alt="" />
+        <img src={Ash} alt="" />
        </header>
   )
 }
